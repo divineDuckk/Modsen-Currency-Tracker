1cab2bab893a255a11521c31fa2551666dd144ce
divineDuckk <assasinsmn@gmail.com>
2024-06-22 17:05:08 +0300

    feat: add little animation in header

--- src/components/Header/NavBar/styled.ts
+++ src/components/Header/NavBar/styled.ts
@@ -1,5 +1,7 @@
 import styled, { css } from 'styled-components'
 
+const NAV_ELEM_TRANSITION = 0.2
+
 export const NavContainer = styled.ul`
   display: flex;
   ${({ theme }) => css`
@@ -11,4 +13,9 @@ export const NavContainer = styled.ul`
 `
 export const NavElement = styled.li`
   cursor: pointer;
+  transition: ${NAV_ELEM_TRANSITION}s;
+  &:hover {
+    opacity: 1;
+    filter: drop-shadow(0 0 0.75rem ${({ theme }) => theme.colors['white']});
+  }
 `
